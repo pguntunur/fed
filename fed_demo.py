22208ad5269178e528200c3a3869639059c44dd9
Pranav <pg1653@nyu.edu>
2021-02-26 15:53:14 +0530

    testing fed on tc_usr_data

--- fed_demo.py
+++ fed_demo.py
@@ -1,4 +1,4 @@
-import fed
+# import fed
 
 # Load model
 model, tokenizer = fed.load_models("microsoft/DialoGPT-large")
@@ -20,6 +20,16 @@ import pandas as pd
 tc_usr_dataTSV = "../../../chatbot-eval/tc_usr_data.tsv"
 tc_fed_scores = "../../../chatbot-eval/tc_fed_scores.tsv"
 
-tc_usr_dataTSV_r = pd.read_csv(tc_usr_dataTSV, sep="\t")
+# tc_usr_dataTSV = "../chatbot-eval/tc_usr_data.tsv"
+# tc_fed_scores = "../chatbot-eval/tc_fed_scores.tsv"
 
-print(tc_usr_dataTSV_r.head(10))
+tc_usr_dataTSV_r = pd.read_csv(tc_usr_dataTSV, sep="\t")
+out_df = pd.DataFrame(tc_usr_dataTSV_r["context"])
+out_df["fed_scores"] = ""
+
+# Change text format of chateval dataset according to input in fed.py
+for i, row in out_df.iterrows():
+    out_df.at[i,'fed_scores'] = fed.evaluate(row["context"], model, tokenizer)
+
+with open(tc_fed_scores,'w') as write_tsv:
+    write_tsv.write(out_df.to_csv(sep='\t', index=False))
