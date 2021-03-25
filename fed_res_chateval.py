8ad802bc4a5eb446b8fe030f44084888da350e61
Pranav <pg1653@nyu.edu>
2021-03-25 13:27:36 +0530

    making changes to evaluate model responses

--- fed_res_chateval.py
+++ fed_res_chateval.py
@@ -7,22 +7,22 @@ model, tokenizer = fed.load_models("microsoft/DialoGPT-large")
 print("Model loaded.")
 
 # Using this model with our chat-eval data
-prompt_dataCSV = "../../../chatbot-eval/turksData/prompts.csv"
-prompt_fed_scores = "../../../chatbot-eval/turksData/prompt_fed_scores.tsv"
+res_dataCSV = "../../../chatbot-eval/turksData/model_responses.csv"
+res_fed_scores = "../../../chatbot-eval/turksData/res_fed_scores.tsv"
 
-prompt_data = pd.read_csv(prompt_dataCSV, sep=",")
-out_df = pd.DataFrame(prompt_data)
+res_data = pd.read_csv(res_dataCSV, sep=",")
+out_df = pd.DataFrame(res_data)
 out_df["fed_scores"] = ""
 
 # potentially delete escape characters and see if that affects the FED model?
-tqdm.pandas(desc="Evaluating with FED")
+tqdm.pandas(desc="Evaluating model responses with FED")
 def fed_apply(context_str):
     return fed.evaluate(context_str, model, tokenizer)
 
 out_df['fed_scores'] = out_df["prompt_text"].progress_apply(fed_apply)
 print("Scores evaluated.")
 
-with open(tc_fed_scores,'w') as write_tsv:
+with open(res_fed_scores,'w') as write_tsv:
     write_tsv.write(out_df.to_csv(sep='\t', index=False))
 
 print("Done.")
